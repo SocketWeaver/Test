0227e96ac1dd5545f31441a759ae4f85735760bc
PeterZ <socketweaver@gmail.com>
2019-12-08 08:46:52 -0800

    Added Rotation2D scene

---
+++ Test/Assets/Scripts/Rotation2D/Movement2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SWNetwork;
+
+public class Movement2D : MonoBehaviour
+{
+    public float rotateSpeed = 20f;
+    NetworkID networkID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkID = GetComponent<NetworkID>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(networkID.IsMine)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Test/Assets/Scripts/Shared/TestSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SWNetwork;
+
+public class TestSpawner : MonoBehaviour
+{
+    public void OnSpawnerReady(bool alreadySetup, SceneSpawner sceneSpawner)
+    {
+        Debug.Log("OnSpawnerReady " + alreadySetup);
+
+        if (!alreadySetup)
+        {
+            if (NetworkClient.Instance.IsHost)
+            {
+                GameObject go = sceneSpawner.SpawnForPlayer(0, 0);
+
+                Debug.Log("OnSpawnerReady created GameObject" + go.name);
+            }
+            else
+            {
+                GameObject go = sceneSpawner.SpawnForPlayer(0, 1);
+
+                Debug.Log("OnSpawnerReady created GameObject" + go.name);
+            }
+
+
+            sceneSpawner.PlayerFinishedSceneSetup();
+        }
+    }
+}
